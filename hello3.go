d96eeb5af1a5be2166dd56a0b336ba7003880505
Arturo Cobos <arturo@co-boss.com>
2016-03-29 21:42:25 -0500

    prueba de creacion y asignación de variables.

---
+++ hello3.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+    //declarando la variable inicial
+    var a string = "inicial"
+    fmt.Println(a)
+    
+//    declarando más de una variable.
+    var b,c,h int = 4,5,0
+    fmt.Println(b,c,h)
+    
+    //Declarando variables con resultado booleano
+    var d = true
+    fmt.Println(d)
+    
+    //Declarando variable e de tipo entero
+    var e int
+    fmt.Println(e)
+    
+    f:= "short"
+    fmt.Println(f)
+    
+}
