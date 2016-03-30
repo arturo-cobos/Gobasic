5c4c3de96a9852c383fd620813a9eb4aa7b28aff
Arturo Cobos <arturo@co-boss.com>
2016-03-29 21:01:00 -0500

    + comment

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,13 @@
+//Hello World!
+
 package main
+//paquete mein es esepcial define un programa ejecutable por si solo.
+
 
 import "fmt"
+//fumt - nombre copartirto para format..
 
+//main declaración función main
 func main() {
-	fmt.Println("Hello, playground")
+    fmt.Println("Hello, playground")
 }
